0e055b4ddd08fe09e0636032d52a692cb7db63a6
Federico.Murru <djfedemurru@icloud.com>
2024-05-15 16:12:47 +0100

    model

--- aulab_post_federico_murru/resources/views/components/navbar.blade.php
+++ aulab_post_federico_murru/resources/views/components/navbar.blade.php
@@ -1,14 +1,11 @@
 <nav class="navbar navbar-expand-sm bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand mb-2" href="#">AulabPost</a>
+        <a class="navbar-brand mb-2" href="{{route('homepage')}}">AulabPost</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
                 <li class="nav-item">
                     <a class="nav-link active" href="#">Link</a>
                 </li>
