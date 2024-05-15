15dd2ab69f5b537b4a4e914341f2c87dfde31f71
Federico.Murru <djfedemurru@icloud.com>
2024-05-15 14:09:52 +0100

    css

--- aulab_post_federico_murru/resources/views/components/layout.blade.php
+++ aulab_post_federico_murru/resources/views/components/layout.blade.php
@@ -3,17 +3,14 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>The Aulab Post</title>
-  @vite(["resources/css/app.css", "resources/js/app.js"])
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+<link href="{{ asset('/style.css') }}" rel="stylesheet">
+<title>The Aulab Post</title>
+@vite(["resources/css/app.css", "resources/js/app.js"])
 
-
-  <script src="https://kit.fontawesome.com/3a3ad33343.js" crossorigin="anonymous"></script>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Nova+Square&family=Rajdhani:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Nova+Square&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Rajdhani:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
 <body class="bg-img">
 <x-navbar>
