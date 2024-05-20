fbc4212e0ddf853022cdf6a800cfa934ba936138
Federico.Murru <djfedemurru@icloud.com>
2024-05-20 18:52:10 +0100

    revisor

--- aulab_post_federico_murru/bootstrap/app.php
+++ aulab_post_federico_murru/bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\UserIsAdmin;
+use App\Http\Middleware\UserIsRevisor;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,7 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'admin' => UserIsAdmin::class
+            'admin' => UserIsAdmin::class,
+            'revisor' => UserIsRevisor::class
+
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
