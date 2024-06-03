cc28c78d6b96054db69047e261dff2f4dc3fa541
Federico.Murru <djfedemurru@icloud.com>
2024-06-03 17:38:07 +0100

    writer controller

--- aulab_post_federico_murru/app/Http/Controllers/WriterController.php
+++ aulab_post_federico_murru/app/Http/Controllers/WriterController.php
@@ -8,12 +8,11 @@ use Illuminate\Support\Facades\Auth;
 class WriterController extends Controller
 {
     public function dashboard(){
-        $articles = Auth::user()->articles()->orderBy('created_at', 'desc')->get();
-    
-        $unrevisionedArticles = $articles->where('is_accepted', '===', NULL);
+        $articles=Auth::user()->articles()->orderBy('created_at','desc')->get();    
         $acceptedArticles = $articles->where('is_accepted', '===', true);
         $rejectedArticles = $articles->where('is_accepted', '===', false);
-    
+        $unrevisionedArticles = $articles->where('is_accepted', '===', NULL);
+
         return view('writer.dashboard', compact('unrevisionedArticles', 'acceptedArticles', 'rejectedArticles'));
     }
 }
